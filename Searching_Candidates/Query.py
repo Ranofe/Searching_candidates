43591de3ed351a8e813ab35120dbccd4b51ae9e3
Ranofe <fededeave@gmail.com>
2022-07-27 19:49:33 -0300

    ended sub_query tests

--- Searching_Candidates/Query.py
+++ Searching_Candidates/Query.py
@@ -12,36 +12,42 @@ class Querier(object):
 
     def read_json(self):
         f = open(self.json_file_path, "r")
-        self.query_dict = json.load(f)
+        self.query_json_dict = json.load(f)
 
     def create_query(self):
-        site_query = "site:linkedin.com/in"
+        if 'Title' in self.query_json_dict:
+            title_query = "intitle:"
+            title_query += self.sub_query_maker(self.query_json_dict['Title'])
 
-        title_query = "intitle:"
-        title_query += self.sub_query_maker(self.query_dict['Title'])
-
-        query = title_query + " " + site_query
+        query = title_query + " " + self.site
         self.google_query = query
         return query
 
     def sub_query_maker(self, sub_query_dict):
         subquery = ""
+        must = 0  # indicates there is a must term
         # Must elements
         if "Must" in sub_query_dict:
-            must = sub_query_dict['Must']
-            subquery += "("
-            for n, element in enumerate(must):
-                subquery += element
-                if n < (len(must) - 1):
-                    subquery += " AND "
-            subquery += ")"
+            if len(sub_query_dict['Must']) > 0:
+                must = sub_query_dict['Must']
+                subquery += "("
+                for n, element in enumerate(must):
+                    subquery += element
+                    if n < (len(must) - 1):
+                        subquery += " AND "
+                subquery += ")"
+                must = 1
         # Options
         if "Optional" in sub_query_dict:
-            optional = sub_query_dict['Optional']
-            subquery += " AND ("  # Start OR
-            for n, element in enumerate(optional):
-                subquery += element
-                if n < (len(optional) - 1):
-                    subquery += " OR "
-            subquery += ")"  # End OR
+            if len(sub_query_dict['Optional']) > 0:
+                optional = sub_query_dict['Optional']
+                if must != 0:
+                    subquery += " AND ("  # Start OR
+                else:
+                    subquery += "("
+                for n, element in enumerate(optional):
+                    subquery += element
+                    if n < (len(optional) - 1):
+                        subquery += " OR "
+                subquery += ")"  # End OR
         return subquery
--- test/unit/test_Query.py
+++ test/unit/test_Query.py
@@ -5,12 +5,12 @@ from Searching_Candidates.Query import Querier
 
 class TestQuery(unittest.TestCase):
     def test_Create_Querier(self):
-        file_path = "./example/query_example.json"
+        file_path = "./test/unit/query_test.json"
         Q = Querier(file_path)
-        self.assertEqual(type(Q.query_dict) is dict, True, "File not read")
+        self.assertEqual(type(Q.query_json_dict) is dict, True, "File not read")
 
     def test_sub_query_maker(self):
-        file_path = "./example/query_example.json"
+        file_path = "./test/unit/query_test.json"
         q_sub_query = {'Must': ['QA Automation'], 'Optional': ['Ssr', 'Sr']}
 
         Q = Querier(file_path)
@@ -23,8 +23,17 @@ class TestQuery(unittest.TestCase):
         result = Q.sub_query_maker(q_sub_query)
         self.assertEqual(result, expected_result, "Sub query without options error")
 
-        # TODO: Tests without must and optinoal
-        # q_sub_query = {'Must': ['QA Automation'], 'Optional':[]}
-        # expected_result = "(QA Automation)"
-        # result = Q.sub_query_maker(q_sub_query)
-        # self.assertEqual(result, expected_result, "Sub query without options error")
+        q_sub_query = {'Must': ['QA Automation'], 'Optional': []}
+        expected_result = "(QA Automation)"
+        result = Q.sub_query_maker(q_sub_query)
+        self.assertEqual(result, expected_result, "Sub query without options error")
+
+        q_sub_query = {'Optional': ['Ssr', 'Sr']}
+        expected_result = "(Ssr OR Sr)"
+        result = Q.sub_query_maker(q_sub_query)
+        self.assertEqual(result, expected_result, "Sub query without must error")
+
+        q_sub_query = {'Must': [], 'Optional': ['Ssr', 'Sr']}
+        expected_result = "(Ssr OR Sr)"
+        result = Q.sub_query_maker(q_sub_query)
+        self.assertEqual(result, expected_result, "Sub query without must error")
